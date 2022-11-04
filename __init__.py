0df4df73a8be4ea2e70b386b24ba6c95e8e1383c
gustavo-dm <gustavothemoraes@gmail.com>
2022-11-04 17:09:02 -0300

    sprint 4, logging and minor adjusts

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from db import db
 
 
-def create_app(): 
+def create_app():
     app = Flask(__name__)
     app.secret_key = 'askjdnaskjdnaksjdn'
     app.config['SESSION_TYPE'] = 'memcached'
--- db.py
+++ db.py
@@ -1,2 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import logging
+
+engine = create_engine('sqlite:///arquesys.db', echo=True,
+                       connect_args={"check_same_thread": False})
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
 db = SQLAlchemy()
+
+
+handler = logging.FileHandler('app.log')
+handler.setLevel(logging.DEBUG)
+
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').addHandler(handler)
